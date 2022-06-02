f2126c51e6f6bfbbfe2dc2de41036f7cf8ff12d9
jaime zuber <jaimezuber99@gmail.com>
2022-06-02 20:36:47 -0300

    msj

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
   get 'lists/:id/add_movie', to: 'lists#add_movie', as: 'add_movie'
   post 'lists/add_movie', to: 'lists#save_movie', as: 'save_movie'
-  resources :lists, only: %i[index show new create] do
+  resources :lists, only: %i[show new create] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: %i[destroy]
